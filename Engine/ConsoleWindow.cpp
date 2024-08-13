1223fb4795a6e7ca7dbdd8f3743ff7031f83dbc0
5Cents <real5centstv@gmail.com>
2024-08-12 19:39:30 -0700

    Fix ConsoleWindow so that users can't close the console using the X.

--- Engine/ConsoleWindow.cpp
+++ Engine/ConsoleWindow.cpp
@@ -27,6 +27,10 @@ bool ConsoleWindow::CreateNewConsole(uint16 bufferLength)
 	{
 		AdjustConsoleBuffer(bufferLength);
 		result = RedirectConsoleIO();
+
+		HWND consoleHandle = GetConsoleWindow();
+		HMENU hmenu = GetSystemMenu(consoleHandle, FALSE);
+		EnableMenuItem(hmenu, SC_CLOSE, MF_GRAYED);
 	}
 	printf("Console Created\n");
 
@@ -57,7 +61,9 @@ void ConsoleWindow::AdjustConsoleBuffer(uint16 bufferLength)
 	CONSOLE_SCREEN_BUFFER_INFO conInfo;
 	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &conInfo);
 	if (conInfo.dwSize.Y < bufferLength)
+	{
 		conInfo.dwSize.Y = bufferLength;
+	}
 	SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), conInfo.dwSize);
 }
 
@@ -71,25 +77,39 @@ bool ConsoleWindow::ReleaseConsole()
 
 	// Redirect STDIN to NUL
 	if (freopen_s(&fp, "NUL:", "r", stdin) != 0)
+	{
 		result = false;
+	}
 	else
+	{
 		setvbuf(stdin, NULL, _IONBF, 0);
+	}
 
 	// Redirect STDOUT to NUL
 	if (freopen_s(&fp, "NUL:", "w", stdout) != 0)
+	{
 		result = false;
+	}
 	else
+	{
 		setvbuf(stdout, NULL, _IONBF, 0);
+	}
 
 	// Redirect STDERR to NUL
 	if (freopen_s(&fp, "NUL:", "w", stderr) != 0)
+	{
 		result = false;
+	}
 	else
+	{
 		setvbuf(stderr, NULL, _IONBF, 0);
+	}
 
 	// Detach from console
 	if (!FreeConsole())
+	{
 		result = false;
+	}
 
 	return result;
 }
@@ -101,24 +121,40 @@ bool ConsoleWindow::RedirectConsoleIO()
 
 	// Redirect STDIN if the console has an input handle
 	if (GetStdHandle(STD_INPUT_HANDLE) != INVALID_HANDLE_VALUE)
+	{
 		if (freopen_s(&fp, "CONIN$", "r", stdin) != 0)
+		{
 			result = false;
+		}
 		else
+		{
 			setvbuf(stdin, NULL, _IONBF, 0);
+		}
+	}
 
 	// Redirect STDOUT if the console has an output handle
 	if (GetStdHandle(STD_OUTPUT_HANDLE) != INVALID_HANDLE_VALUE)
 		if (freopen_s(&fp, "CONOUT$", "w", stdout) != 0)
+		{
 			result = false;
+		}
 		else
+		{
 			setvbuf(stdout, NULL, _IONBF, 0);
+		}
 
 	// Redirect STDERR if the console has an error handle
 	if (GetStdHandle(STD_ERROR_HANDLE) != INVALID_HANDLE_VALUE)
+	{
 		if (freopen_s(&fp, "CONOUT$", "w", stderr) != 0)
+		{
 			result = false;
+		}
 		else
+		{
 			setvbuf(stderr, NULL, _IONBF, 0);
+		}
+	}
 
 	// Make C++ standard streams point to console as well.
 	std::ios::sync_with_stdio(true);
